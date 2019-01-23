fa7314cd669ec4d23bf89279b7a28a3240111f33
Rumi <refik@basigovci.com>
2019-01-23 18:31:00 +0100

    <link rel="stylesheet" href="node_modules/electron-tabs/electron-tabs.css">

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const {app, BrowserWindow, Menu} = require('electron');
-const TabGroup = require("electron-tabs");
+
 let mainWindow;
 let menuTemplate = [
     {
@@ -16,12 +16,6 @@ let menuTemplate = [
         ]
     }
 ];
-let tabGroup = new TabGroup();
-let tab = tabGroup.addTab({
-    title: "Electron",
-    src: "http://electron.atom.io",
-    visible: true
-});
 function appInit () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600,icon: __dirname + '/assets/icon512.png'})
